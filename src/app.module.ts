c766d0f74433a0688bcb88e3dc01762d15acbfee
bedhilzz <fadhil49@gmail.com>
2026-01-17 08:24:57 +0700

    Add Short URL module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ShortUrlModule } from './short-url.module';
 
 @Module({
   imports: [
@@ -9,8 +10,9 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, // available everywhere
       envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
     }),
+    ShortUrlModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
